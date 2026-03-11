0903d622d9f383118aee14a8d16a7475c53306f0
mozu-blo <lue.pomedog@iCloud.com>
2026-03-11 14:33:33 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,10 @@
 import type { NextConfig } from "next";
 
-const isGithubPages = process.env.GITHUB_ACTIONS === "true";
-
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: isGithubPages ? "/hakumiru" : "",
-  assetPrefix: isGithubPages ? "/hakumiru/" : "",
 };
 
 export default nextConfig;
